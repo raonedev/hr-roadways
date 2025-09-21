a64fd06d9344e3541e396451d50147d355e82da3
raonedev <kumaraman33063@gmail.com>
2025-09-21 19:46:21 +0530

    clean half

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -49,11 +49,14 @@ class _HomeScreenState extends State<HomeScreen> {
   Future<void> _launchTermsUrl() async {
     const url =
         'https://raonedev.github.io/hHrRoadways_privacyPolicy/privay-policy-web.html';
-    if (!await launchUrl(
-      Uri.parse(url),
-      mode: LaunchMode.externalApplication,
-    )) {
-      throw Exception('Could not launch $url');
+    final uri = Uri.parse(url);
+
+    if (await canLaunchUrl(uri)) {
+      if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+        throw Exception('Could not launch $url');
+      }
+    } else {
+      throw Exception('Cannot launch $url');
     }
   }
 
@@ -144,8 +147,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Text(
-                      (fromController.text.isNotEmpty ||
-                              toController.text.isNotEmpty)
+                      (fromController.text.isNotEmpty || toController.text.isNotEmpty)
                           ? "\nPlease try with different routes"
                           : "\n\nNote: This is not an official Haryana Roadways app. \nData is sourced from https://hartrans.gov.in/ for public convenience.",
                       style: TextStyle(
@@ -173,180 +175,179 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget buildSearchCard() {
     return Padding(
-      padding: const EdgeInsets.only(left: 16.0, right: 16, top: kToolbarHeight),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.black, AppColors.dark, Colors.purple],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0.5, 0.8, 1],
-              ),
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withValues(alpha: 0.2),
-                  spreadRadius: 2,
-                  blurRadius: 10,
-                  offset: const Offset(0, 5),
-                ),
-              ],
+      padding: const EdgeInsets.only(
+        left: 16.0,
+        right: 16,
+        top: kToolbarHeight,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black, AppColors.dark, Colors.purple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.5, 0.8, 1],
+          ),
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.2),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: const Offset(0, 5),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "From",
+              style: TextStyle(
+                color: AppColors.background,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Autocomplete<String>(
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text.isEmpty) {
+                  return const Iterable<String>.empty();
+                }
+                return locations.where(
+                  (option) => option.toLowerCase().contains(
+                    textEditingValue.text.toLowerCase(),
+                  ),
+                );
+              },
+              onSelected: (String selection) {
+                fromController.text = selection;
+              },
+              fieldViewBuilder: (
+                context,
+                controller,
+                focusNode,
+                onFieldSubmitted,
+              ) {
+                controller.text = fromController.text;
+                return TextFormField(
+                  controller: controller,
+                  focusNode: focusNode,
+                  decoration: InputDecoration(
+                    hintText: "Enter origin",
+                    hintStyle: TextStyle(
+                      color: AppColors.dark.withValues(alpha: 0.5),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 2,
+                      horizontal: 16,
+                    ),
+                    fillColor: AppColors.background,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            Row(
               children: [
                 Text(
-                  "From",
+                  "To",
                   style: TextStyle(
                     color: AppColors.background,
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-                const SizedBox(height: 8),
-                Autocomplete<String>(
-                  optionsBuilder: (TextEditingValue textEditingValue) {
-                    if (textEditingValue.text.isEmpty) {
-                      return const Iterable<String>.empty();
-                    }
-                    return locations.where(
-                      (option) => option.toLowerCase().contains(
-                        textEditingValue.text.toLowerCase(),
-                      ),
-                    );
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.swap_vert_rounded, color: Colors.white),
+                  onPressed: () {
+                    setState(() {
+                      final String temp = fromController.text.trim();
+                      fromController.text = toController.text;
+                      toController.text = temp;
+                    });
                   },
-                  onSelected: (String selection) {
-                    fromController.text = selection;
-                  },
-                  fieldViewBuilder: (
-                    context,
-                    controller,
-                    focusNode,
-                    onFieldSubmitted,
-                  ) {
-                    controller.text = fromController.text;
-                    return TextFormField(
-                      controller: controller,
-                      focusNode: focusNode,
-                      decoration: InputDecoration(
-                        hintText: "Enter origin",
-                        hintStyle: TextStyle(
-                          color: AppColors.dark.withValues(alpha: 0.5),
-                        ),
-                        contentPadding: EdgeInsets.symmetric(
-                          vertical: 2,
-                          horizontal: 16,
-                        ),
-                        fillColor: AppColors.background,
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  children: [
-                    Text(
-                      "To",
-                      style: TextStyle(
-                        color: AppColors.background,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Spacer(),
-                    IconButton(
-                      icon: Icon(Icons.swap_vert_rounded, color: Colors.white),
-                      onPressed: () {
-                        setState(() {
-                          final String temp = fromController.text.trim();
-                          fromController.text = toController.text;
-                          toController.text = temp;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Autocomplete<String>(
-                  optionsBuilder: (TextEditingValue textEditingValue) {
-                    if (textEditingValue.text.isEmpty) {
-                      return const Iterable<String>.empty();
-                    }
-                    return locations.where(
-                      (option) => option.toLowerCase().contains(
-                        textEditingValue.text.toLowerCase(),
-                      ),
-                    );
-                  },
-                  onSelected: (String selection) {
-                    toController.text = selection;
-                  },
-                  fieldViewBuilder: (
-                    context,
-                    controller,
-                    focusNode,
-                    onFieldSubmitted,
-                  ) {
-                    controller.text = toController.text;
-                    return TextFormField(
-                      controller: controller,
-                      focusNode: focusNode,
-                      decoration: InputDecoration(
-                        hintText: "Enter destination",
-                        hintStyle: TextStyle(
-                          color: AppColors.dark.withValues(alpha: 0.5),
-                        ),
-                        contentPadding: EdgeInsets.symmetric(
-                          vertical: 2,
-                          horizontal: 16,
-                        ),
-                        fillColor: AppColors.background,
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(height: 24),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      context.read<RoutesSearchProvider>().searchRoutes(
-                        fromController.text.trim(),
-                        toController.text.trim(),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                    child: const Text(
-                      "Search",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            const SizedBox(height: 8),
+            Autocomplete<String>(
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text.isEmpty) {
+                  return const Iterable<String>.empty();
+                }
+                return locations.where(
+                  (option) => option.toLowerCase().contains(
+                    textEditingValue.text.toLowerCase(),
+                  ),
+                );
+              },
+              onSelected: (String selection) {
+                toController.text = selection;
+              },
+              fieldViewBuilder: (
+                context,
+                controller,
+                focusNode,
+                onFieldSubmitted,
+              ) {
+                controller.text = toController.text;
+                return TextFormField(
+                  controller: controller,
+                  focusNode: focusNode,
+                  decoration: InputDecoration(
+                    hintText: "Enter destination",
+                    hintStyle: TextStyle(
+                      color: AppColors.dark.withValues(alpha: 0.5),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 2,
+                      horizontal: 16,
+                    ),
+                    fillColor: AppColors.background,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  context.read<RoutesSearchProvider>().searchRoutes(
+                    fromController.text.trim(),
+                    toController.text.trim(),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Text(
+                  "Search",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
